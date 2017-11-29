7a438492898e8aee12ddb7cb3f18ca633a27d45e
ArtemD <dokuchaev.artem1@gmail.com>
2017-11-29 16:36:14 +0200

    CancelSubmit() now works

--- JS console/script.js
+++ JS console/script.js
@@ -3,22 +3,22 @@
                 }
                 document.getElementById( "btn" ).onclick = someFunc;*/
 
-    function cancelSubmit(event) {
-        event.preventDefault;
+   function cancelSubmit(event) {
+        event.preventDefault();
     }
 
     function showField() {
-        console.log(document.getElementById("txt1").value);
+        console.log(document.getElementById("PreCodeText").value);
     }
 
     function insertField(event) {
-        event.preventDefault;
-        var PreCode = document.getElementById('Result'),
-            Result = document.getElementById('txt1').value;
 
-        PreCode.innerHTML = Result;
+        cancelSubmit(event);
+        showField();
+
+        var Result = document.getElementById('Result'),
+            PreCode = document.getElementById('PreCodeText').value;
+        Result.innerHTML = PreCode;
     }
 
     btn.addEventListener('click', insertField);
-    //btn.addEventListener( 'click', insertField );
-    //cancleSubmit();
