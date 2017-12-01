b76b608db3701383c11fdfeecac06f4e07549d3e
ArtemD <dokuchaev.artem1@gmail.com>
2017-12-01 21:31:16 +0200

    Error handling without syntax errors

--- JS console/script.js
+++ JS console/script.js
@@ -1,24 +1,23 @@
-    /*function someFunc () {
-                    alert( document.getElementById("txt" ).value);
-                }
-                document.getElementById( "btn" ).onclick = someFunc;*/
+ /*function someFunc () {
+                 alert( document.getElementById("txt" ).value);
+             }
+             document.getElementById( "btn" ).onclick = someFunc;*/
+function cancelSubmit ( event ) {
+     event.preventDefault();
+}
+function showField () {
+     console.log(document.getElementById ( "PreCodeText" ).value);
+}
+function insertField ( event ) {
+    cancelSubmit( event );
+    showField ();
+    var Result = document.getElementById ( 'Result' ),
+        PreCode = document.getElementById ( 'PreCodeText' ).value;
+    Result.innerHTML = Result.value + '\n' + PreCode;
 
-   function cancelSubmit(event) {
-        event.preventDefault();
-    }
+    var Script = document.createElement ( "script" );
+    Script.textContent = ("try{ " + PreCode + "}catch(err){alert(\'Ошибка\' + err.name + \':\' + err.message + \'\\n\' + err.stack);}");
+    document.head.appendChild( Script );
+}
 
-    function showField() {
-        console.log(document.getElementById("PreCodeText").value);
-    }
-
-    function insertField(event) {
-
-        cancelSubmit(event);
-        showField();
-
-        var Result = document.getElementById('Result'),
-            PreCode = document.getElementById('PreCodeText').value;
-        Result.innerHTML = PreCode;
-    }
-
-    btn.addEventListener('click', insertField);
+btn.addEventListener ( 'click', insertField );
