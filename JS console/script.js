7f94af37f6603e01a658dc3dd393d82e3dae7a27
ArtemD <dokuchaev.artem1@gmail.com>
2017-12-02 17:22:58 +0200

    eval now returns function value

--- JS console/script.js
+++ JS console/script.js
@@ -1,38 +1,37 @@
- /*function someFunc () {
-                 alert( document.getElementById("txt" ).value);
-             }
-             document.getElementById( "SubmitButton" ).onclick = someFunc;*/
 function cancelSubmit( event ) {
+
      event.preventDefault();
+     console.dir( event );
+
 }
 
 function ctrlTrigger( event ) {
+
     if ( event.charCode === 10 ) {
         document.getElementById('SubmitButton').click();
     }
+
 }
 function insertField( event ) {
     cancelSubmit( event );
+
     var Result = document.getElementById( 'Result' ),
         PreCode = document.getElementById( 'PreCodeText' );
     Result.innerHTML = Result.value + '\n' + PreCode.value + '\n';
 
     try {
-        eval( PreCode.value );
+        eval( PreCode );
+        Result.innerHTML = Result.value + eval( PreCode.value );
+
+
+
     } catch ( err ) {
+
         var ErrorText = 'Error:\n' + err.name + ':' + err.message + '\n' + err.stack; 
         console.log( ErrorText );
         Result.innerHTML = Result.value + '\n' + ErrorText + '\n';
-    }
 
-/*  //inserting script into a document; won't handle syntax errors;
-    var Script = document.createElement ( "script" );
-    Script.textContent = ( PreCode );
-    try{
-        document.head.appendChild( Script );
-    } catch (err){
-        alert( 'Ошибка' + err.name + ':' + err.message + '\n' + err.stack );
-    }*/
+    }
 }
 
 SubmitButton.addEventListener( 'click', insertField );
