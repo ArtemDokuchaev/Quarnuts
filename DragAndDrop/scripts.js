6dfad91ec026c3ec98cc2d398c68059230e31f12
ArtemD <dokuchaev.artem1@gmail.com>
2017-12-25 14:09:27 +0300

    drag and drop modified: easy version

--- DragAndDrop/scripts.js
+++ DragAndDrop/scripts.js
@@ -1,6 +1,8 @@
 'use strict'
 var Clicked = false;
-document.getElementById( 'CreateButton' ).addEventListener( 'click', function( event ) {
+document.getElementById( 'CreateButton' ).addEventListener( 'click', dragAndDrop );
+document.body.addEventListener( 'click', deleteObject );
+function dragAndDrop( event ){
 	
 	if ( ! document.getElementById( 'DraggableObject' ) ) {
 			
@@ -14,13 +16,13 @@ document.getElementById( 'CreateButton' ).addEventListener( 'click', function( e
 		var shiftX = event.pageX - coords.left;
 		var shiftY = event.pageY - coords.top;
 
-		DraggableObject.style.zIndex = 1000; // над другими элементами
+		DraggableObject.style.zIndex = 1000; // above other objects
 
 		DraggableObject.addEventListener( 'click', function( event ){
-		 	
-		 	Clicked = !Clicked;
-		 	coords = getCoords( DraggableObject );
-		 	shiftX = event.pageX - coords.left;
+			
+			Clicked = !Clicked;
+			coords = getCoords( DraggableObject );
+			shiftX = event.pageX - coords.left;
 			shiftY = event.pageY - coords.top;
 
 		});
@@ -36,19 +38,34 @@ document.getElementById( 'CreateButton' ).addEventListener( 'click', function( e
 		};
 
 	}
-} );
 
+}
 function moveAt( e, shiftX, shiftY ) {
-	setTimeout( function(){
+		
 		DraggableObject.style.left = e.pageX - shiftX + 'px';
 		DraggableObject.style.top = e.pageY - shiftY + 'px';
-	}, 100);
-}
 
-function getCoords( elem ) { // кроме IE8-
+}
+function getCoords( elem ) { // except IE8-
+
 	var box = elem.getBoundingClientRect();
 	return {
+
 		top: box.top + pageYOffset,
 		left: box.left + pageXOffset
+
 	};
+
+}
+function deleteObject( event ){
+
+	if ( event.pageX >= DeleteButton.offsetLeft &&
+		event.pageX <= DeleteButton.offsetLeft + DeleteButton.offsetWidth &&		
+		event.pageY >= DeleteButton.offsetTop &&
+		event.pageY <= DeleteButton.offsetHeight + DeleteButton.offsetTop &&
+		!Clicked ) {
+		document.body.removeChild( document.getElementById( 'DraggableObject' ) );
+
+	}
+
 }
